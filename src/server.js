0798c6712c58b357fadf32ede61c03aeea8eaa19
Marius <mariushaf@gmail.com>
2022-08-24 09:15:16 +0300

    npm xpress cors morgan

---
+++ src/server.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log('server.js');
+
+const express = require('express')
+const app = express()
+
+app.get('/', (req,res)=> {
+    res.send=('Hello World')
+})
+
+
+app.listen('3000')
