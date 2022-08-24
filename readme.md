fc63802748570b5f6ec7e5110baa1e4d2ff5626a
Marius <mariushaf@gmail.com>
2022-08-24 10:41:26 +0300

    user add works

--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 CREATE NEW PROJECT
 
-1. Inicijuojam npm
+1. Inicijuojam npm npm init
 2. prisidedam githuba
-   3.parsisiusti express, cors
-   4.sukurti serveri
+   3.parsisiusti express, cors npm i express cors
+   4.sukurti serveri creatinam src/server.js
    5.GET / - hello world
 3. parsisiusti ir panaudoti morgan
 4. prisideti public papke su front end
--- src/server.js
+++ src/server.js
@@ -1,12 +1,66 @@
 'use strict';
-console.log('server.js');
 
+const { response, request } = require('express');
 const express = require('express')
+const cors = require('cors')
+const morgan = require('morgan')
 const app = express()
 
-app.get('/', (req,res)=> {
-    res.send=('Hello World')
+//middleware
+app.use(cors())
+app.use(morgan('dev'))
+//Gautame request body galetume matyti json atsiustus duomenis turim ijungti json atkodavima
+app.use(express.json())
+// Atkoduoti request body form data
+// app.use(express.urlencoded({extended: false}))
+
+//DATA
+const users = [
+    {id: 3, name: 'James'},
+    {id: 2, name: 'Serbentautas'},
+    {id: 1, name: 'Lenteja'},
+]
+
+
+//routes
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World</h1>')
+})
+
+//GET /api/users -> grazinti visu useriu masyva json
+app.get('/api/users', (request, response) => {
+    response.json(users)
+})
+// GET /api/users/3 -> grazinti useri kurio id yra 3 (ne pagal indexa)
+app.get('/api/users/3', (request,response) => {
+    const searchIndex = 3
+    const foundUser = users.find((uObj) => uObj.id === searchIndex)
+    if (foundUser) {
+     response.json(foundUser)   
+    } else {
+        response.status(404).json({msg: 'not found'})
+    }
+    
+})
+
+// POST /api/users -> sukurti nauja useri su gautu name ir prideti prie esamu, grazinti 201 statusa su zinute
+app.post('/api/users', (request, response)=> {
+    //Add new user
+   const newUserName = request.body.name
+   const newUserId = users.length +1
+   const newUserObj = {
+    id: newUserId,
+    name: newUserName
+   }
+   users.push(newUserObj)
+    response.json({msg: `${newUserName} Have been added !`})
 })
 
 
-app.listen('3000')
+//404
+app.use((req,res)=> {
+    res.status(404).json({msg: 'Sorry page not found'})
+})
+
+const port = 3000
+app.listen(port, ()=> console.log(`server is listening on port 3000`))
